107dd16afd659e8e33f884efbc600b72a8626b6f
Tạ Thúc Trung Kiên <tathuctrungkien2003@gmail.com>
2025-02-03 11:14:14 +0700

    Handle link using default browser

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+"use client";
 import Image from "next/image";
 
 export default function Home() {
+  const openInBrowser = (url: string) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -23,13 +28,15 @@ export default function Home() {
           <li>Save and see your changes instantly.</li>
         </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <div
+          className="flex gap-4 items-center flex-col sm:flex-row cursor-pointer"
+          onClick={() =>
+            openInBrowser(
+              "https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            )
+          }
+        >
+          <a className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
             <Image
               className="dark:invert"
               src="https://nextjs.org/icons/vercel.svg"
