2a7186b62cbd607fef13e1fce765eb6f84bb33aa
Tạ Thúc Trung Kiên <tathuctrungkien2003@gmail.com>
2025-02-04 11:04:44 +0700

    Use <a> props instead of window.open()

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,6 @@
 import Image from "next/image";
 
 export default function Home() {
-  const openInBrowser = (url: string) => {
-    // if (window.matchMedia("(display-mode: standalone)").matches) {
-    //   window.location.href = url;
-    // } else {
-    //   window.open(url, "_blank", "noopener,noreferrer");
-    // }
-    window.location.href = url;
-  };
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -34,14 +25,12 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="cursor-pointer rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            onClick={() =>
-              openInBrowser(
-                "https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              )
-            }
-          >
+            <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
             <Image
               className="dark:invert"
               src="https://nextjs.org/icons/vercel.svg"
@@ -50,27 +39,23 @@ export default function Home() {
               height={20}
             />
             Deploy now
-          </a>
-          <a
-            className="cursor-pointer rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            onClick={() =>
-              openInBrowser(
-                "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              )
-            }
-          >
+            </a>
+            <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
             Read our docs
-          </a>
+            </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
-          className="cursor-pointer flex items-center gap-2 hover:underline hover:underline-offset-4"
-          onClick={() =>
-            openInBrowser(
-              "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            )
-          }
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <Image
             aria-hidden
@@ -82,12 +67,10 @@ export default function Home() {
           Learn
         </a>
         <a
-          className="cursor-pointer flex items-center gap-2 hover:underline hover:underline-offset-4"
-          onClick={() =>
-            openInBrowser(
-              "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            )
-          }
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <Image
             aria-hidden
@@ -99,12 +82,10 @@ export default function Home() {
           Examples
         </a>
         <a
-          className="cursor-pointer flex items-center gap-2 hover:underline hover:underline-offset-4"
-          onClick={() =>
-            openInBrowser(
-              "https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            )
-          }
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <Image
             aria-hidden
